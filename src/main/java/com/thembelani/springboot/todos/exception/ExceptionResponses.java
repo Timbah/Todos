a96743d7de80dc6fc37cab4bf1c93df353ea28af
Thembelani <thembelaningema20@gmail.com>
2025-12-06 08:41:30 +0200

    Global Exception Handling

---
+++ src/main/java/com/thembelani/springboot/todos/exception/ExceptionResponses.java
@@ -0,0 +1,32 @@
+package com.thembelani.springboot.todos.exception;
+
+
+public class ExceptionResponses {
+    private int status;
+    private String message;
+    private long timestamp;
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
