b99e7d00af6d8e57429cbd189b59135ee706c127
Thembelani <thembelaningema20@gmail.com>
2025-12-06 09:58:23 +0200

    Update Password - User controller

--- src/main/java/com/thembelani/springboot/todos/controller/UserController.java
+++ src/main/java/com/thembelani/springboot/todos/controller/UserController.java
@@ -4,8 +4,10 @@ import com.thembelani.springboot.todos.entity.User;
 import com.thembelani.springboot.todos.request.PasswordUpdateRequest;
 import com.thembelani.springboot.todos.response.UserResponse;
 import com.thembelani.springboot.todos.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User REST API Endpoints",description = "Operations related to info about current user")
@@ -19,16 +21,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "User information",description = "Get current user info")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo(){
         return userService.getUserInfo();
     }
 
+    @Operation(summary = "Delete user",description = "Delete current user")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping()
     public void deleteUser(){
         userService.deleteUser();
     }
 
+    @Operation(summary = "Password Update",description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/password")
     public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest)
             throws Exception{
