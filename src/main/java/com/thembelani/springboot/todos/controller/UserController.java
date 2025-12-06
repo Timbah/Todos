54db240cbc36053e2eda977bb155ff3596fdcb27
Thembelani <thembelaningema20@gmail.com>
2025-12-06 09:54:46 +0200

    Update Password - User controller

--- src/main/java/com/thembelani/springboot/todos/controller/UserController.java
+++ src/main/java/com/thembelani/springboot/todos/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.thembelani.springboot.todos.controller;
 
 import com.thembelani.springboot.todos.entity.User;
+import com.thembelani.springboot.todos.request.PasswordUpdateRequest;
 import com.thembelani.springboot.todos.response.UserResponse;
 import com.thembelani.springboot.todos.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User REST API Endpoints",description = "Operations related to info about current user")
 @RestController
@@ -28,5 +27,11 @@ public class UserController {
     @DeleteMapping()
     public void deleteUser(){
         userService.deleteUser();
+    }
+
+    @PutMapping("/password")
+    public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest)
+            throws Exception{
+        userService.updatePassword(passwordUpdateRequest);
     }
 }
