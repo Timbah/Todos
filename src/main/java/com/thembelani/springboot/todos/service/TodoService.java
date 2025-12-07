dd080f6c6795adf04408c80c9be8564bc46cc960
Thembelani <thembelaningema20@gmail.com>
2025-12-07 17:35:46 +0200

    TODOs - Fetch todos

--- src/main/java/com/thembelani/springboot/todos/service/TodoService.java
+++ src/main/java/com/thembelani/springboot/todos/service/TodoService.java
@@ -3,6 +3,9 @@ package com.thembelani.springboot.todos.service;
 import com.thembelani.springboot.todos.request.TodoRequest;
 import com.thembelani.springboot.todos.response.TodoResponse;
 
+import java.util.List;
+
 public interface TodoService {
+    List<TodoResponse> getAllTodos();
     TodoResponse createTodo(TodoRequest todoRequest);
 }
